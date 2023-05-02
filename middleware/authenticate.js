f55101787595ad4625ab8f21757597a40d65b5b8
Sahil18718 <119488054+Sahil18718@users.noreply.github.com>
2023-05-02 17:05:54 +0530

    .

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,17 +1,23 @@
 const jwt = require("jsonwebtoken")
 // const {UserModel} = require("../model/usermodel")
 const {blacklistModel} = require("../model/blacklistmodel")
-
+const Redis = require("ioredis")
+const redis=new Redis()
 
 const auth = async (req,res,next)=>{
     const token = req.headers.authorization.split(" ")[1]
 
 
     try {
-        const blacklisted = await blacklistModel.find({"token":token})
+        const blacklisted = await redis.get({"token":token})
         
         if (blacklisted) {
             return res.status(400).json({"msg":"Unauthorized1"})
+        }else{
+            const black = await blacklistModel.find({"token":token})
+            if(black[0]){
+                return res.status(400).json({"msg":"Unauthorized2"})
+            }
         }
     } catch (error) {
         res.status(400).send({"msg":"Blacklisted"})
